e2889ecc8333774a7f017248c92abe22d22cff5b
hpant <hpant@shutterfly.com>
2022-02-21 17:35:49 -0600

    Code to run in Windows to push image in slack

---
+++ slack-chrome-integration/README.md
@@ -0,0 +1,27 @@
+# Introduction
+
+
+## Pre-requisite:
+- Need to have python3
+- Need to have **SLACK_BOT_TOKEN** and **Channel_id** to push text/image in slack channel
+- export SLACK_BOT_TOKEN=""xoxb-xxxxxxxxxx" -> git bash
+- Run python script
+
+
+### Required Modules
+- Install pip module:
+
+	`pip install slack_sdk`
+	`pip install selenium`
+	`pip install webdriver_manager`
+
+
+
+### Steps to setup Slack App (Need SLACK_BOT_TOKEN)
+- Go to socket mode and turn **on** 
+- Got to **OAuth and Permissions** and select below configuration to get necessary permissions to post image in channel.
+
+
+![image](https://user-images.githubusercontent.com/10596429/154865755-e62de7cb-a1c7-40e2-8b4b-95ff1897d64b.png)
+
+
---
+++ slack-chrome-integration/slack-chrome-integration-for-windows.py
@@ -0,0 +1,51 @@
+
+#coding=utf-8                                                                                                                                                                              
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import logging
+import os
+import boto3
+
+# Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+URL = ''
+
+driver.get(URL)
+
+S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
+#driver.set_window_size(S('Width'),S('Height'))
+driver.set_window_size(1920, 1080)
+# May need manual adjustment                                                                                                                
+driver.find_element_by_tag_name('body').screenshot('web_screenshot.png')
+
+driver.quit()
+# WebClient instantiates a client that can call API methods
+# When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+client = client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+logger = logging.getLogger(__name__)
+# The name of the file you're going to upload
+file_name = r"C:\Users\user\Documents\projects\projects\jira-integration\web_screenshot.png"
+# ID of channel that you want to upload file to
+channel_id= ""
+
+#client.chat_postMessage(channel=channel_id, text="hello")
+try:
+    # Call the files.upload method using the WebClient
+    # Uploading files requires the `files:write` scope
+    result = client.files_upload(
+        channels=channel_id,
+        initial_comment="Job Status:",
+        file=file_name,
+    )
+    # Log the result
+    logger.info(result)
+
+except SlackApiError as e:
+    logger.error("Error uploading file: {}".format(e))
