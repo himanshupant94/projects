a49a8fea84bf6a6215e6d1d2f49a28473ed19c73
hpant <hpant@shutterfly.com>
2022-02-22 10:39:28 -0600

    parameterized variables and used hyperlink

--- slack-chrome-integration/slack-chrome-integration-for-windows.py
+++ slack-chrome-integration/slack-chrome-integration-for-windows.py
@@ -15,8 +15,7 @@ from slack_sdk.errors import SlackApiError
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-URL = ''
-
+URL = os.environ.get("URL")
 driver.get(URL)
 
 S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
@@ -31,21 +30,20 @@ driver.quit()
 client = client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 logger = logging.getLogger(__name__)
 # The name of the file you're going to upload
-file_name = r"C:\Users\user\Documents\projects\projects\jira-integration\web_screenshot.png"
+file_name = r"C:\Users\hpant\Documents\projects\projects\jira-integration\web_screenshot.png"
 # ID of channel that you want to upload file to
-channel_id= ""
-
+channel_id= os.environ.get("channel_id")
 #client.chat_postMessage(channel=channel_id, text="hello")
 try:
     # Call the files.upload method using the WebClient
     # Uploading files requires the `files:write` scope
     result = client.files_upload(
         channels=channel_id,
-        initial_comment="Job Status:",
+        initial_comment="Job Status: <"+ URL +"+|Ops360 link>",
         file=file_name,
     )
     # Log the result
-    logger.info(result)
+    logger.info(result) 
 
 except SlackApiError as e:
     logger.error("Error uploading file: {}".format(e))
