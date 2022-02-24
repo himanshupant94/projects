a8f936a5df91d10cf9422557bf0c0c4bc8c8ffe1
hpant <hpant@shutterfly.com>
2022-02-24 15:17:03 -0600

    add verify false parameter

--- jira-integration/basic_jira_creation/helper.py
+++ jira-integration/basic_jira_creation/helper.py
@@ -4,7 +4,7 @@ from jira import JIRA
 class JiraHelper:
 
     def __init__(self, username, password, server):
-        self.jira = JIRA(basic_auth=(username, password), options={"server": server, 'verify':False})
+        self.jira = JIRA(options={"server": server, "verify":False},basic_auth=(username, password))
 
     def create_issue(self, test_data):
         issue_key = self.jira.create_issue(fields=test_data)
